961991eebc68fe65e0dc599dd9e0b651be291759
scripthost12 <nuno007soares@outlook.pt>
2020-12-30 16:59:43 +0000

    added stuff

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,6 +10,15 @@ class Fun(commands.Cog):
         m = await ctx.send("doggo")
         self.bot.get_cog("Uninvoke").create_unload(ctx.message, lambda: m.delete())
 
+    @commands.is_owner()
+    @commands.command(name="wb")
+    async def send_webhook(self, ctx, name, av, *, content):
+        await ctx.message.delete()
+        webhook = await self.bot.get_webhook_for_channel(ctx.channel)
+        webhook: discord.Webhook
+        if webhook is not None:
+            await webhook.send(content, username=name, avatar_url=av if av.startswith("http") else None)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
