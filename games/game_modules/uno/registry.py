fce6fd3e886941a398a4ec62dc55de1a07d93066
scripthost12 <nuno007soares@outlook.pt>
2021-01-18 22:00:38 +0000

    bugfixing

--- games/game_modules/uno/registry.py
+++ games/game_modules/uno/registry.py
@@ -10,7 +10,8 @@ class CardType:
     @classmethod
     def other_place_attempt(cls, this, other, game):
         return this.color == other.color or \
-               (this.number == other.number and other.number is not None) or other.color is None
+               (this.number == other.number and other.number is not None and
+                not issubclass(other.cls, AdversaryPayCardType)) or other.color is None
 
     @classmethod
     def get_user_friendly(cls, this):
