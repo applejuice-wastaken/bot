bf7b2043fff3441c930d5222679718a5eb4c3654
scripthost12 <nuno007soares@outlook.pt>
2021-01-25 19:57:30 +0000

    more bugfixing yay

--- games/GamePlayer.py
+++ games/GamePlayer.py
@@ -1,6 +1,9 @@
 import discord
 from discord.abc import Messageable
 
+class Absorber:
+    async def add_reaction(self, *_, **__):
+        pass
 
 class GamePlayer(Messageable):
     async def _get_channel(self):
@@ -9,9 +12,10 @@ class GamePlayer(Messageable):
     async def send(self, *args, **kwargs):
         if self.able_to_send_messages:
             try:
-                await super(GamePlayer, self).send(*args, **kwargs)
+                return await super(GamePlayer, self).send(*args, **kwargs)
             except:
                 self.able_to_send_messages = False
+                return Absorber()
 
     def __init__(self, user, bound_channel):
         self.bound_channel = bound_channel
